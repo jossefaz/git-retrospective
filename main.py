adc765d38767b6a5feb66f79eec8a8ddc06c8ab6
jossefaz <jossefaz@protonmail.com>
2024-05-02 12:39:12 +0300

    Add more filter option

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
 from datetime import timezone
-
 from github import Github
 import pandas as pd
 from datetime import datetime
 
 
-def fetch_commit_data(start_date, end_date, repo_name, token, base_url):
+def fetch_commit_data(start_date, end_date, repo_name, token, base_url, target_users_email=None, title_keywords=None, main_branch='develop'):
     g = Github(base_url=base_url, login_or_token=token)
     repo = g.get_repo(repo_name)
 
     start_date = datetime.strptime(start_date, '%Y-%m-%d').replace(tzinfo=timezone.utc)
     end_date = datetime.strptime(end_date, '%Y-%m-%d').replace(tzinfo=timezone.utc)
 
-    pulls = repo.get_pulls(state='closed', sort='created', base='develop')
+    pulls = repo.get_pulls(state='closed', sort='created', base=main_branch)
     commits_info = []
 
     for pull in pulls:
         if pull.merged_at and start_date <= pull.merged_at <= end_date:
+            if title_keywords and not any(keyword in pull.title for keyword in title_keywords):
+                continue
             for commit in pull.get_commits():
-                commit_date = commit.commit.committer.date.date()  # commit_date is offset-naive
+                if target_users_email and commit.commit.committer.email not in target_users_email:
+                    continue
+                commit_date = commit.commit.committer.date.date()
                 commits_info.append((commit_date, commit.commit.committer.email))
 
     return commits_info
 
 
-def generate_commit_report(start_date, end_date, repo_name, token, base_url):
-    commits_info = fetch_commit_data(start_date, end_date, repo_name, token, base_url)
+def generate_commit_report(start_date, end_date, repo_name, token, base_url, target_users_email=None,
+                           title_keywords=None, main_branch='develop'):
+    commits_info = fetch_commit_data(start_date, end_date, repo_name, token, base_url, target_users_email,
+                                     title_keywords, main_branch)
     df = pd.DataFrame(commits_info, columns=['date', 'contributor'])
     report = df.pivot_table(index='date', columns='contributor', aggfunc='size', fill_value=0)
     report.to_csv('commit_report.csv')
@@ -43,7 +48,11 @@ def add_stats(file_path):
 
 
 if __name__ == '__main__':
-    generate_commit_report('2024-01-01', '2024-05-01', 'org/repo',
-                           'PAT TOKEN',
-                           'https://git.illumina.com/api/v3')
+    generate_commit_report('2024-01-01', '2024-05-01', 'REPO',
+                           'TOKEN',
+                           'IF GITHUB.COM NO NEED T SPECIFY THIS IS ONLY FOR ENTERPRISE',
+                           target_users_email=['some_user@mail.com'],
+                           title_keywords=['SOME TITLE KEY WORD OF THE PR'],
+                           main_branch='develop'
+                           )
     add_stats('commit_report.csv')
